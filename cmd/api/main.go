c23306d27408ab06b411481face6161d739b8bd8
Gabriel Pozo <jackgris2@gmail.com>
2024-09-08 20:25:16 -0300

    feat: implement graceful shutdown for server
    Signed-off-by: Gabriel Pozo <jackgris2@gmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
 	"runtime"
 	"strconv"
+	"syscall"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackgris/backend-notification-APIs/internal/loggin"
@@ -47,13 +50,39 @@ func run(ctx context.Context, log *logs.Logger) error {
 		os.Exit(1)
 	}
 
-	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "Server started at port", port)
-	err = http.ListenAndServe(":"+portEnv, mux)
-	if err != nil {
-		return err
+	srv := &http.Server{
+		Addr:    ":" + portEnv,
+		Handler: mux,
 	}
 
-	log.Info(ctx, "Server shutdown OK")
+	serverErrors := make(chan error, 1)
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0), "Server started at port", port)
+
+		serverErrors <- srv.ListenAndServe()
+	}()
+
+	// -------------------------------------------------------------------------
+	// Shutdown
+
+	select {
+	case err := <-serverErrors:
+		return fmt.Errorf("server error: %w", err)
+
+	case sig := <-shutdown:
+		log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
+		defer log.Info(ctx, "shutdown", "status", "shutdown complete", "signal", sig)
+
+		ctx, cancel := context.WithTimeout(ctx, time.Microsecond*500)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			return fmt.Errorf("could not stop server gracefully: %w", err)
+		}
+	}
 
 	return nil
 }
@@ -61,13 +90,13 @@ func run(ctx context.Context, log *logs.Logger) error {
 func connectDB(ctx context.Context, log *logs.Logger) *pgx.Conn {
 	conn, err := pgx.Connect(ctx, os.Getenv("DATABASE_URL"))
 	if err != nil {
-		log.Error(ctx, fmt.Sprintf("Unable to connect to database: %v\n", err))
+		log.Error(ctx, "Unable to connect to database", "ERROR", err)
 		os.Exit(1)
 	}
 
 	err = conn.Ping(ctx)
 	if err != nil {
-		log.Error(ctx, fmt.Sprintf("Connection err: %v\n", err))
+		log.Error(ctx, "Connection database", "ERROR", err)
 		os.Exit(1)
 	}
 
